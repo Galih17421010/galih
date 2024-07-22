85de18ef9dc8673b4ebb7d024a02c368a528788e
Galih Agus Saputra <agussaputragalih@gmail.com>
2024-07-22 14:08:46 +0700

    add page create article

--- resources/views/backend/pages/blog/article/create.blade.php
+++ resources/views/backend/pages/blog/article/create.blade.php
@@ -1,6 +1,19 @@
 @extends('backend.index')
 @section('title','Create Article')
-@section('content')
-Create article
-<article>Test Content</article>
+@section('css')
+    <link rel="stylesheet" href={{asset("assets/css/typography.css")}} class="template-customizer-core-css" />
 @endsection
+@section('content')
+<div class="col-12">
+    <div class="card">
+      <h5 class="card-header">Full Editor</h5>
+      <div class="card-body">
+        <div id="full-editor">
+          <h6>Quill Rich Text Editor</h6>
+          <p> Cupcake ipsum dolor sit amet. Halvah cheesecake chocolate bar gummi bears cupcake. Pie macaroon bear claw. Soufflé I love candy canes I love cotton candy I love. </p>
+        </div>
+      </div>
+    </div>
+</div>
+@endsection
+
